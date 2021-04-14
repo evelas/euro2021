47392a743a71063406dede39ea079f4d83e0cf1a
Evelas <evelas94@gmail.com>
2021-04-14 12:34:26 +0300

    test hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './scss/app.scss';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import createAppStore from './redux/redux-store';
 import { PersistGate } from 'redux-persist/integration/react';
 const { persistor, store } = createAppStore();
 
 ReactDOM.render(
-  <Router basename={process.env.PUBLIC_URL}>
+  <Router >
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
@@ -20,5 +20,3 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
-// import * as React from "react";
-// import * as ReactDOM from "react-dom";
