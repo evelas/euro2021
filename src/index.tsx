0f50251cbdeb65b60eab376f2b8a10fa6b593733
Evelas <evelas94@gmail.com>
2021-04-16 12:44:54 +0300

    redux-persist out of bounds

--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,11 @@ import App from './App';
 import './scss/app.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-// import createAppStore from './redux/redux-store';
-// import { PersistGate } from 'redux-persist/integration/react';
-// const { persistor, store } = createAppStore();
 import store from './redux/redux-store';
 
 ReactDOM.render(
   <Router basename={process.env.PUBLIC_URL + "/dashboard/"}>
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate> */}
       <App />
     </Provider>
   </Router>,
--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,26 +1,12 @@
 import { applyMiddleware, createStore, compose } from 'redux';
-// import thunkMiddleware from 'redux-thunk';
 import reducers from './reducers';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './sagas/sagas';
-
-// import { persistStore, persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-// };
-// const persistedReducer = persistReducer(persistConfig, reducers);
-
-//const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-// const saga = createSagaMiddleware();
 
 const saga = createSagaMiddleware();
 // eslint-disable-next-line
 // @ts-ignore
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const store = createStore(reducers, composeEnhancers(applyMiddleware(saga)));
 
 saga.run(rootSaga);
@@ -30,15 +16,3 @@ window.store = store;
 
 export default store;
 
-// const configureStore = () => {
-//   const store = createStore(persistedReducer, applyMiddleware(thunkMiddleware, saga));
-//   // eslint-disable-next-line
-//   // @ts-ignore
-//   window.store = store;
-
-//   saga.run(rootSaga);
-//   const persistor = persistStore(store);
-//   return { store, persistor };
-// };
-
-// export default configureStore;
