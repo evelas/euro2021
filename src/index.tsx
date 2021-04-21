9e70ee7707d2316296f7d2e20bd3a56dd26eaba6
Evelas <evelas94@gmail.com>
2021-04-21 15:40:39 +0300

    editorconfig

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './scss/app.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import { Provider } from 'react-redux';
-// import store from './redux/redux-store';
 
 import createAppStore from './redux/redux-store';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -15,8 +13,8 @@ const { persistor, store } = createAppStore();
 ReactDOM.render(
   <Router >
     <Provider store={store}>
-       <PersistGate loading={null} persistor={persistor}>
-        <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App/>
       </PersistGate>
     </Provider>
   </Router>,
